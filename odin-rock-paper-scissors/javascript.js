aeccbeba34b2c0576c75d2df0c7e635e3d68c802
Adam Z <adamzubel@gmail.com>
2026-05-29 14:05:56 +0300

    add while loop to play whole 5 rounds game

--- odin-rock-paper-scissors/javascript.js
+++ odin-rock-paper-scissors/javascript.js
@@ -1,5 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
+let isGameOn = true;
 
 // Function that provides computer's choice 0 - Scissors, 1 - Rock, 2 - Paper
 function getComputerChoice() {
@@ -24,36 +26,53 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-function playRound(computerGesture, playerGesture) {
-    if (computerGesture === playerGesture) {
+// Function to play one round and compare choices
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
         console.log("Tie, no points!");
     }
-    else if (computerGesture === 0 && playerGesture === 1) {
+    else if (computerSelection === 0 && playerSelection === 1) {
         console.log("You Win! Rock beats Scissors!");
         playerScore++;
     }
-    else if (computerGesture === 1 && playerGesture === 2) {
+    else if (computerSelection === 1 && playerSelection === 2) {
         console.log("You Win! Paper beats Rock!");
         playerScore++;
     }
-    else if (computerGesture === 2 && playerGesture === 0) {
+    else if (computerSelection === 2 && playerSelection === 0) {
         console.log("You Win! Scissors beat Paper!");
         playerScore++;
     }
-    else if (computerGesture === 0 && playerGesture === 2) {
+    else if (computerSelection === 0 && playerSelection === 2) {
         console.log("You Lose! Scissors beat Paper!");
         computerScore++;
     }
-    else if (computerGesture === 1 && playerGesture === 0) {
+    else if (computerSelection === 1 && playerSelection === 0) {
         console.log("You Lose! Rock beats Scissors!");
         computerScore++;
     }
-    else if (computerGesture === 2 && playerGesture === 1) {
+    else if (computerSelection === 2 && playerSelection === 1) {
         console.log("You Lose! Paper beats Rock!");
         computerScore++;
     }
 }
 
-playRound(getComputerChoice(), getPlayerChoice());
-console.log(playerScore)
-console.log(computerScore)
+// While Loop for 5 rounds, with the winner declaration at the end
+while (isGameOn) {
+    playRound(getComputerChoice(), getPlayerChoice());
+    roundNumber++;
+    console.log("Your Score: " + playerScore)
+    console.log("Computer Score: " + computerScore)
+    if (roundNumber >= 6) {
+        isGameOn = false;
+        if (playerScore > computerScore) {
+            console.log("End of the Game! You've won!!!")
+        }
+        else if (playerScore < computerScore) {
+            console.log("End of the Game! You've lost!!!")
+        }
+        else {
+            console.log("End of the Game! Tie!!!")
+        }
+    }
+}
