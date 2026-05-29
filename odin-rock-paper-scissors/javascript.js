6ca404718d1e1b063f1a4a629ac03452c521fd67
Adam Z <adamzubel@gmail.com>
2026-05-29 13:48:38 +0300

    add starting score and choice comparision

--- odin-rock-paper-scissors/javascript.js
+++ odin-rock-paper-scissors/javascript.js
@@ -1,9 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+
 // Function that provides computer's choice 0 - Scissors, 1 - Rock, 2 - Paper
 function getComputerChoice() {
     return computerChoice = Math.floor(Math.random() * 3);
 }
-
-console.log(getComputerChoice());
 
 // Function that provides player's choice, 'P' for paper, 'R' for rock, 'S' for scissors
 function getPlayerChoice() {
@@ -23,7 +24,36 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-console.log(getPlayerChoice());
+function playRound(computerGesture, playerGesture) {
+    if (computerGesture === playerGesture) {
+        console.log("Tie, no points!");
+    }
+    else if (computerGesture === 0 && playerGesture === 1) {
+        console.log("You Win! Rock beats Scissors!");
+        playerScore++;
+    }
+    else if (computerGesture === 1 && playerGesture === 2) {
+        console.log("You Win! Paper beats Rock!");
+        playerScore++;
+    }
+    else if (computerGesture === 2 && playerGesture === 0) {
+        console.log("You Win! Scissors beat Paper!");
+        playerScore++;
+    }
+    else if (computerGesture === 0 && playerGesture === 2) {
+        console.log("You Lose! Scissors beat Paper!");
+        computerScore++;
+    }
+    else if (computerGesture === 1 && playerGesture === 0) {
+        console.log("You Lose! Rock beats Scissors!");
+        computerScore++;
+    }
+    else if (computerGesture === 2 && playerGesture === 1) {
+        console.log("You Lose! Paper beats Rock!");
+        computerScore++;
+    }
+}
 
-let playerScore = 0;
-let computerScore = 0;
+playRound(getComputerChoice(), getPlayerChoice());
+console.log(playerScore)
+console.log(computerScore)
