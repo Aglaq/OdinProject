629f1524a3f0708e8e5369e945d85dca757c9d67
Adam Z <adamzubel@gmail.com>
2026-06-05 16:05:49 +0300

    add refresh score

--- odin-rock-paper-scissors/javascript.js
+++ odin-rock-paper-scissors/javascript.js
@@ -2,6 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 1;
 let isGameOn = true;
+let resultText = "";
 
 // Function that provides computer's choice 0 - Scissors, 1 - Rock, 2 - Paper
 function getComputerChoice() {
@@ -29,32 +30,34 @@ function getPlayerChoice() {
 // Function to play one round and compare choices
 function playRound(computerSelection, playerSelection) {
     if (computerSelection === playerSelection) {
-        console.log("Tie, no points!");
+        resultText = "Tie, no points!";
     }
     else if (computerSelection === 0 && playerSelection === 1) {
-        console.log("You Win! Rock beats Scissors!");
+        resultText = "You Win! Rock beats Scissors!";
         playerScore++;
     }
     else if (computerSelection === 1 && playerSelection === 2) {
-        console.log("You Win! Paper beats Rock!");
+        resultText = "You Win! Paper beats Rock!";
         playerScore++;
     }
     else if (computerSelection === 2 && playerSelection === 0) {
-        console.log("You Win! Scissors beat Paper!");
+        resultText = "You Win! Scissors beat Paper!";
         playerScore++;
     }
     else if (computerSelection === 0 && playerSelection === 2) {
-        console.log("You Lose! Scissors beat Paper!");
+        resultText = "You Lose! Scissors beat Paper!";
         computerScore++;
     }
     else if (computerSelection === 1 && playerSelection === 0) {
-        console.log("You Lose! Rock beats Scissors!");
+        resultText = "You Lose! Rock beats Scissors!";
         computerScore++;
     }
     else if (computerSelection === 2 && playerSelection === 1) {
+        resultText = "You Lose! Paper beats Rock!";
         console.log("You Lose! Paper beats Rock!");
         computerScore++;
     }
+    refreshScore();
 }
 
 const rockButton = document.createElement("button");
@@ -70,26 +73,19 @@ scissorsButton.addEventListener("click", () => playRound(getComputerChoice(), 0)
 scissorsButton.textContent = "SCISSORS";
 body.appendChild(scissorsButton);
 
+const result = document.createElement("div");
+const score = document.createElement("div");
+body.appendChild(result);
+body.appendChild(score);
 
+function refreshScore() {
+    result.textContent = `${resultText}`;
+    score.textContent = `Your Score: ${playerScore} Computer Score: ${computerScore}`
 
-// While Loop for 5 rounds, with the winner declaration at the end
-// function playGame() {
-//     while (isGameOn) {
-//         playRound(getComputerChoice(), getPlayerChoice());
-//         roundNumber++;
-//         console.log("Your Score: " + playerScore)
-//         console.log("Computer Score: " + computerScore)
-//         if (roundNumber >= 6) {
-//             isGameOn = false;
-//             if (playerScore > computerScore) {
-//                 console.log("End of the Game! You've won!!!")
-//             }
-//             else if (playerScore < computerScore) {
-//                 console.log("End of the Game! You've lost!!!")
-//             }
-//             else {
-//                 console.log("End of the Game! Tie!!!")
-//             }
-//         }
-//     }
-// }
+    if (playerScore === 5) {
+        alert("End of the Game! You've won!!!")
+    }
+    else if (computerScore === 5) {
+        alert("End of the Game! You've lost!!!")
+    }
+}
