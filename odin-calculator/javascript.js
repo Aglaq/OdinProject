6be8c3d0f1501d62ea75776e5e39a18aa832ac7d
Adam Z <adamzubel@gmail.com>
2026-08-17 20:43:53 +0300

    add functionality

--- odin-calculator/javascript.js
+++ odin-calculator/javascript.js
@@ -1,100 +1,145 @@
 // Basic math functions
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => (b === 0 ? "ERROR" : a / b);
 
+// Counting
 function operate(firstNum, secondNum, ope) {
-    if (ope == "+") {
-        return add(firstNum, secondNum);
-    } else if (ope == "-") {
-        return subtract(firstNum, secondNum);
-    } else if (ope == "*") {
-        return multiply(firstNum, secondNum);
-    } else if (ope == "/") {
-        return divide(firstNum, secondNum);
-    };
-};
+  if (ope == "+") {
+    return add(firstNum, secondNum);
+  } else if (ope == "-") {
+    return subtract(firstNum, secondNum);
+  } else if (ope == "*") {
+    return multiply(firstNum, secondNum);
+  } else if (ope == "/") {
+    return divide(firstNum, secondNum);
+  }
+}
 
-function reset() {
-    firstNumber = 0;
+// Clear function
+function clear() {
+  firstNumber = 0;
+  secondNumber = NaN;
+  operator = "";
+  const display = document.getElementById("display");
+  display.textContent = Number(Number(firstNumber).toFixed(5));
+}
+
+function numberButtonClick(num) {
+  if (operator == "") {
+    if (firstNumber == 0) {
+      firstNumber = num;
+    } else {
+      firstNumber = firstNumber.toString() + `${num}`;
+    }
+    display.textContent = Number(Number(firstNumber).toFixed(5));
+  } else {
+    if (Number.isNaN(secondNumber) || secondNumber == 0) {
+      secondNumber = num;
+    } else {
+      secondNumber = secondNumber.toString() + `${num}`;
+    }
+    display.textContent = Number(Number(secondNumber).toFixed(5));
+  }
+}
+
+function signButtonClick(sign) {
+  if (Number.isNaN(secondNumber)) {
+    display.textContent = Number(Number(firstNumber).toFixed(5));
+    operator = sign;
+  } else {
+    firstNumber = operate(
+      parseInt(firstNumber),
+      parseInt(secondNumber),
+      operator,
+    );
+    display.textContent = Number(Number(firstNumber).toFixed(5));
+    operator = sign;
+    secondNumber = NaN;
+  }
+}
+
+// Clear button
+const button_clear = document.getElementById("button_clear");
+button_clear.addEventListener("click", () => {
+  clear();
+});
+
+// Number buttons
+const button_zero = document.getElementById("button_zero");
+button_zero.addEventListener("click", () => {
+  numberButtonClick(0);
+});
+const button_one = document.getElementById("button_one");
+button_one.addEventListener("click", () => {
+  numberButtonClick(1);
+});
+const button_two = document.getElementById("button_two");
+button_two.addEventListener("click", () => {
+  numberButtonClick(2);
+});
+const button_three = document.getElementById("button_three");
+button_three.addEventListener("click", () => {
+  numberButtonClick(3);
+});
+const button_four = document.getElementById("button_four");
+button_four.addEventListener("click", () => {
+  numberButtonClick(4);
+});
+const button_five = document.getElementById("button_five");
+button_five.addEventListener("click", () => {
+  numberButtonClick(5);
+});
+const button_six = document.getElementById("button_six");
+button_six.addEventListener("click", () => {
+  numberButtonClick(6);
+});
+const button_seven = document.getElementById("button_seven");
+button_seven.addEventListener("click", () => {
+  numberButtonClick(7);
+});
+const button_eight = document.getElementById("button_eight");
+button_eight.addEventListener("click", () => {
+  numberButtonClick(8);
+});
+const button_nine = document.getElementById("button_nine");
+button_nine.addEventListener("click", () => {
+  numberButtonClick(9);
+});
+
+// Sign buttons for opearator
+const button_plus = document.getElementById("button_plus");
+button_plus.addEventListener("click", () => {
+  signButtonClick("+");
+});
+const button_minus = document.getElementById("button_minus");
+button_minus.addEventListener("click", () => {
+  signButtonClick("-");
+});
+const button_asterix = document.getElementById("button_asterix");
+button_asterix.addEventListener("click", () => {
+  signButtonClick("*");
+});
+const button_slash = document.getElementById("button_slash");
+button_slash.addEventListener("click", () => {
+  signButtonClick("/");
+});
+
+// Equal button
+const button_equal = document.getElementById("button_equal");
+button_equal.addEventListener("click", () => {
+  if (Number.isNaN(secondNumber)) {
     secondNumber = 0;
-    operator = "";
-    result = 0;
-    calculate();
-}
+    operator = "+";
+  }
+  firstNumber = operate(
+    parseInt(firstNumber),
+    parseInt(secondNumber),
+    operator,
+  );
+  display.textContent = Number(Number(firstNumber).toFixed(5));
+  secondNumber = NaN;
+});
 
-function calculate() {
-
-    let firstNumber = 0;
-    let secondNumber = 0;
-    let operator = "";
-    let result = 0;
-
-    const display = document.getElementById("display");
-    display.textContent = "0";
-
-    const button_one = document.getElementById("button_one")
-    button_one.addEventListener("click", () => {
-        if (operator == "") {
-            if (firstNumber == 0) {
-                firstNumber = 1;
-                display.textContent = firstNumber;
-            } else {
-                firstNumber = firstNumber.toString() + "1";
-                display.textContent = firstNumber;
-            };
-        }
-        else {
-            if (secondNumber == 0) {
-                secondNumber = 1;
-                display.textContent = secondNumber;
-            } else {
-                secondNumber = secondNumber.toString() + "1";
-                display.textContent = secondNumber;
-            };
-        };
-    });
-    const button_two = document.getElementById("button_two")
-    button_two.addEventListener("click", () => {
-        if (operator == "") {
-            if (firstNumber == 0) {
-                firstNumber = 2;
-                display.textContent = firstNumber;
-            } else {
-                firstNumber = firstNumber.toString() + "2";
-                display.textContent = firstNumber;
-            };
-        }
-        else {
-            if (secondNumber == 0) {
-                secondNumber = 2;
-                display.textContent = secondNumber;
-            } else {
-                secondNumber = secondNumber.toString() + "2";
-                display.textContent = secondNumber;
-            };
-        };
-    });
-
-    const button_plus = document.getElementById("button_plus")
-    button_plus.addEventListener("click", () => {
-        operator = "+";
-        firstNumber = operate(parseInt(firstNumber), parseInt(secondNumber), operator);
-        secondNumber = 0;
-        display.textContent = firstNumber;
-    });
-
-    const button_equal = document.getElementById("button_equal")
-    button_equal.addEventListener("click", () => {
-        display.textContent = operate(parseInt(firstNumber), parseInt(secondNumber), operator);
-    });
-}
-
-calculate();
-
-// const button_one = document.querySelector("button_one")
-// button_one.addEventListener("click", display());
-
-// result =
-//     console.log(operate(5, 56, "+"));
+clear();
